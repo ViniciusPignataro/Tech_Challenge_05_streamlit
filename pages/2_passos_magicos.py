4bf8b8b06242a6884dcca6a63f7fb1149a398399
Vinicius <viniciuspignataro@gmail.com>
2025-01-21 11:57:06 -0300

    FEAT: teste tabs

--- pages/2_passos_magicos.py
+++ pages/2_passos_magicos.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+from tabs.passos_magicos import sobre_tab
 
 def main():
     # Definindo um título para a página    
@@ -8,17 +8,18 @@ def main():
     # Criação de abas
     tab1, tab2, tab3 = st.tabs(['Sobre', 'Relatórios', 'Base de dados'])
 
+
     with tab1:
-        st.markdown('''
-                    * INTRODUZIR TEXTO SOBRE A PASSOS MÁGICOS, SEPARANDO MAIS SUBHEADERS PARA MISSAO, VISÃO E VALORES * \n
-    	            ''')
+        sobre_tab.show()
+    # with tab1:
+    #     st.markdown('''
+    #                 * INTRODUZIR TEXTO SOBRE A PASSOS MÁGICOS, SEPARANDO MAIS SUBHEADERS PARA MISSAO, VISÃO E VALORES * \n
+    # 	            ''')
 
     with tab2:
         st.markdown('''
                     * INSERIR SOBRE PEDE E RELATÓRIOS *
                     ''')
-        
-        st.image("assets/img/petroleo.png", caption='')
 
     with tab3:
         st.markdown('''
