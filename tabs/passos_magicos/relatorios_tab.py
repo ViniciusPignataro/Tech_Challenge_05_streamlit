c6f6135600cee6dd483b42e89591a3f082fea8f9
Vinicius <viniciuspignataro@gmail.com>
2025-01-22 02:31:04 -0300

    ADD: tab relatorios

--- tabs/passos_magicos/relatorios_tab.py
+++ tabs/passos_magicos/relatorios_tab.py
@@ -4,4 +4,20 @@ def show():
     st.markdown('''
                 A **:red[PEDE]**, ou **:red[Pesquisa Extensiva do Desenvolvimento Educacional]** é uma avaliação realizada pela ONG **:red[Passos Mágicos]**, visando monitorar e avaliar o progresso educacional dos beneficiados pelos seus programas, fornecendo dados e informações sobre o desempenho dos alunos.
                 
+                A ONG **:red[Passos Mágicos]** utiliza uma métrica chamada **:red[INDE (Índice Nacional de Desenvolvimento Educacional)]**, utilizado para avaliar os alunos baseando-se em indicadores relacionados a aspectos psicossociais, acadêmicos e psicopedagógicos, utilizado para definir o Indicador do Ponto de Virada. \n
+
+                Os indicadores se dividem em **:red[3]** dimensões principais:
+                :one: **:red[Dimensão acadêmica:]** abrangendo os indicadores **:red[IAN]**, **:red[IDA]** e **:red[IEG]**
+                :two: **:red[Dimensão psicossocial:]** abrangendo os indicadores **:red[IAA]** e **:red[IPS]**
+                :three: **:red[Dimensão psicopedagógica:]** abrangendo os indicadores **:red[IPP]** e **:red[IPV]**
+
+                Esse conjunto de indicadores, dos quais avaliam diversos aspectos do desenvolvimento educacional do aluno, são pontuados de forma a calcular o **:red[INDE]**. Os indicadores mencionados estão definidos abaixo para melhor entendimento:
+
+                * **:red[Indicador de Adequação de Nível (IAN):]** Analisa se o aluno está num nível adequado de ensino para sua idade;
+                * **:red[Indicador de Desenvolvimento Acadêmico (IDA):]** Avalia o conhecimento do aluno nas disciplinas estabelecidas;
+                * **:red[Indicador de de Engajamento (IEG):]** Registra a participação do aluno em ações de voluntariado nas atividades escolares;
+                * **:red[Indicador de Autoavaliação (IAA):]** Consiste em uma avaliação própria do aluno, estabelecendo como o mesmo se sente em relação à aspectos de sua vida e experiência cotidiana;
+                * **:red[Indicador Psicossocial (IPS):]** Consiste em avaliar o desenvolvimento psicológico e de interações sociais do aluno;
+                * **:red[Indicador Psicopedagógico (IPP):]** Avalia aspectos psicopedagógicos que podem influenciar no aprendizado dos alunos;
+                * **:red[Indicador do Ponto de Virada (IPV):]** Identifica um estágio do desenvolvimento do aluno, do qual demonstra ativamente estar consciente da importância da educação, do saber e do aprender, demonstrando capacidade emocional e acadêmica, integrado aos valores da associação.
                 ''')
